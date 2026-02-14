80b8cea61f31a93ce8fa955b7bf02fbcd1071933
Vishal Pijdurkar <vishalpijdurkar99@gmail.com>
2026-02-14 14:04:29 +0530

    added the next and prev functionality in carousel component

--- carouselComponentUsingReact/src/App.jsx
+++ carouselComponentUsingReact/src/App.jsx
@@ -1,10 +1,26 @@
+import { useState } from 'react'
 import './App.css'
+import Carousel from './componets/Carousel'
+
+const carouselImges = [
+  "https://images.unsplash.com/photo-1532274402911-5a369e4c4bb5?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1470770841072-f978cf4d019e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "https://images.unsplash.com/photo-1527489377706-5bf97e608852?q=80&w=2159&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+]
 
 function App() {
+  const [index, setIndex] = useState(0);
 
+  const next = ()=>{
+    setIndex(prev=>(prev+1)%carouselImges.length)
+  }
+
+  const previous= ()=>{
+    setIndex(prev=>prev===0?carouselImges.length-1:prev-1)
+  }
   return (
     <>
-      <h1>Carousel</h1>
+      <Carousel imgUrl={carouselImges[index]} next={next}  previous={previous}/>
     </>
   )
 }
--- carouselComponentUsingReact/src/componets/Carousel/index.jsx
+++ carouselComponentUsingReact/src/componets/Carousel/index.jsx
@@ -1,12 +1,13 @@
+import './index.css'
 
-function Carousel({imgUrl}) {
+function Carousel({imgUrl, next, previous}) {
     return (
         <>
         <div className="carousel-container">
-            <img src={imgUrl} alt="img"/>
+            <img src={imgUrl} alt="img" className="carousel-img"/>
             <div className="btn-container">
-                <button className="btn left-btn"><img src="" alt="prev" className="btn-icon"/></button>
-                <button className="btn right-btn"><img src="" alt="prev" className="btn-icon"/></button>
+                <button className="btn left-btn" onClick={()=>next()}>prev</button>
+                <button className="btn right-btn" onClick={()=>previous()}>next</button>
             </div>
         </div>
         </>
